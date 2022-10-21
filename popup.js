7d26e8253d0ef91934a3be97f2d0dfc162c473f2
187jjay187 <187jjay187@gmail.com>
2022-10-21 11:21:02 +0200

    fix linter errors

--- popup.js
+++ popup.js
@@ -72,6 +72,7 @@ const projectSix = {
 
 projects.push(projectSix);
 
+// eslint-disable-next-line no-unused-vars
 function projectClick(event) {
   const clickedId = event.target.id;
 
@@ -99,7 +100,7 @@ function projectClick(event) {
       const img = document.createElement('img');
       img.setAttribute('src', projects[i].img);
       img.className = 'popupImg';
-      img.setAttribute('alt','Project image');
+      img.setAttribute('alt', 'Project image');
       imageContainer.appendChild(img);
 
       const careersTitle = document.createElement('h3');
@@ -130,14 +131,15 @@ function projectClick(event) {
       link.appendChild(seeLive);
 
       const seeSource = document.createElement('button');
-      seeSource.className = 'seeSource'
+      seeSource.className = 'seeSource';
       seeSource.innerHTML = '<button type="button" id="btnSource" class="btn">See Source <i class="fa fa-github"></i></i></button ';
       link.appendChild(seeSource);
     }
   }
 }
 
-function closeWindow() { 
+// eslint-disable-next-line no-unused-vars
+function closeWindow() {
   const iconClose = document.getElementsByClassName('popupSection');
   iconClose[0].style.display = 'none';
   iconClose[0].remove();
